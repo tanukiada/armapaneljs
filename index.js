c80d979110918bb9b9b98abb97db2961f66e4421
ada taylor <admin@adataylor.io>
2026-02-13 00:26:29 -0600

    added jwt based auth

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { GameDig } = require('gamedig');
 const Docker = require('dockerode');
-
+const argon2 = require('argon2');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
@@ -9,7 +11,54 @@ app.use(express.json());
 
 let docker = new Docker({socketPath: '/var/run/docker.sock'});
 
+const username = process.env.db_user;
+const password = process.env.db_pass;
+const host = process.env.db_host;
+const dbPort = 5432;
+
+const { Client } = require('pg');
+const config = {
+    user: username,
+    password: password,
+    host: host,
+    port: dbPort,
+    database: 'postgres'
+};
+
 // Support Functions
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+async function createTablesIfNotExists() {
+    const client = new Client(config);
+    await client.connect();
+
+    const usersQuery = `
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        );
+    `;
+
+    await client.query(usersQuery);
+    console.log('Table created.');
+    await client.end();
+}
+
+createTablesIfNotExists();
+
 async function setContainerStatus(containerName, desiredStatus) {
     const container = docker.getContainer(containerName);
 
@@ -31,12 +80,7 @@ async function setContainerStatus(containerName, desiredStatus) {
     }
 }
 
-
-app.get('/', (req, res) => {
-    res.json({output: 'hello, world!'});
-});
-
-app.get('/api/v1/service/status', (req, res) => {
+app.get('/api/v1/service/status', authenticateToken, (req, res) => {
     GameDig.query({
         type: 'arma3',
         host: '127.0.0.1',
@@ -48,7 +92,7 @@ app.get('/api/v1/service/status', (req, res) => {
     });
 });
 
-app.put('/api/v1/service/status', async (req, res) => {
+app.put('/api/v1/service/status', authenticateToken, async (req, res) => {
     const { status } = req.body;
 
     if (!status) {
@@ -61,6 +105,58 @@ app.put('/api/v1/service/status', async (req, res) => {
     } catch (err) {
         res.status(500).json({ success: false, message: `Server error: ${err.message}` });
     }
+});
+
+app.post('/api/v1/auth/register', async(req, res) => {
+    const client = new Client(config);
+    await client.connect();
+    const username = req.body.username;
+    const hash = await argon2.hash(req.body.password);
+    const query = {
+        text: 'INSERT INTO users (username, password) values ($1, $2)',
+        values: [username, hash],
+    };
+    results = await client.query(query);
+    return res.status(201).json({ message: "Account created"});
+})
+
+app.post('/api/v1/auth/login', async (req, res) => {
+    const client = new Client(config);
+    await client.connect();
+    const username = req.body.username;
+    // const hash = await argon2.hash(req.body.password);
+    // const query = {
+    //     text: 'INSERT INTO users (username, password) values ($1, $2)',
+    //     values: [username, hash],
+    // };
+    const query = {
+        text: 'SELECT * FROM users WHERE username = $1',
+        values: [username],
+    };
+    const password = req.body.password; 
+
+    results = await client.query(query);
+
+    const rows = results.rows;
+    const row = rows[0];
+    const dbUser = row.username;
+    const hashedPW = row.password;
+
+    if(username != dbUser) {
+        return res.status(400).json({ message: "Invalid Credentials"});
+    }
+
+    if (!await argon2.verify(hashedPW, password)) {
+        return res.status(400).json({ message: "Invalid Credentials"});
+    }
+
+    const token = jwt.sign(
+        { uname: username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+
+    res.json({ token });
 });
 
 app.listen(port, () => {
