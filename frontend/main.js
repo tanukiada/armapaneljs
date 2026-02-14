1c15c65302aa40fb332210a1ffcb27e80d4deb89
ada taylor <admin@adataylor.io>
2026-02-14 09:42:02 -0600

    ok maybe i was wrong about the capitalization volume 2

--- frontend/main.js
+++ frontend/main.js
@@ -13,7 +13,7 @@ async function getStatus() {
         if (data.Status !== 'Online') {
             setTimeout(getStatus, 5000);
         }
-        document.querySelector('#data-container').innerHTML = "Server Status: " + data.Status;
+        document.querySelector('#data-container').innerHTML = "Server Status: " + data.status;
     } catch (err) {
         console.error('Error fetching data: ', err.message);
     }
