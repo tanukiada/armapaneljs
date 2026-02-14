9e6494d57a856ed33a20db95a18b2a929aac6b41
ada taylor <admin@adataylor.io>
2026-02-14 02:53:13 -0600

    potential fix

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ const config = {
 // Support Functions
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = req.cookies.token;
 
     if (!token) return res.sendStatus(401).redirect('/frontend/login');
 
