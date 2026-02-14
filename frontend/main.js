793b7208a731a47ade8f73c1d32fc0abcb6a8589
ada taylor <admin@adataylor.io>
2026-02-14 09:27:26 -0600

    refactored functions in main.js to use async await instead of .then

--- frontend/main.js
+++ frontend/main.js
@@ -4,50 +4,39 @@ if(!jwt) {
         window.location.href = 'frontend/login/login.html';
 }
 
-function getStatus() {
-    fetch('https://tanuki.gay/api/v1/service/status', {
-        credentials: 'include'
-    })
-        .then(response => {
-            if (response.status === 401) {
-                window.location.href = 'login.html';
-                return;
-            }
-            return response.json()
+async function getStatus() {
+    try {
+        let reponse = await fetch('https://tanuki.gay/api/v1/service/status', {
+            credentials: 'include'
         })
-        .then(data => {
-            if (data.status !== 'Online') {
-                setTimeout(getStatus, 5000);
-            }
-            document.querySelector("#data-container").append( data.status );
-        })
-        .catch(error => console.error('Error fetching data: ', error));
+        let data = response.json();
+        if (data.status !== 'Online') {
+            setTimeout(getStatus, 5000);
+        }
+        document.querySelector('#data-container').append( data.status );
+    } catch (err) {
+        console.error('Error fetching data: ', err.message);
+    }
 }
 
-function changeState(status) {
-    fetch('https://tanuki.gay/api/v1/service/status', {
+async function changeState(status) {
+    getStatus();
+    try{
+        const response = await fetch('https://tanuki.gay/api/v1/service/status', {
         method: 'PUT',
         credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({status: status})
-    })
-        .then(response => {
-            if (response.status === 401) {
-                localStorage.removeItem('token');
-                window.location.href = 'login.html';
-            }
+        body: JSON.stringify({ status: status })
         })
-        .then(response => {
-            if(!response.ok) {
-                return Promise.reject(new Error(`HTTP error! status: ${response.status}`));
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('There was an error with the PUT request: ', error);
-        });
+        if (!response.ok) {
+            console.error('Failed to get response. Please report to your local ada.');
+        }
+        return response.json();
+    } catch (err) {
+        console.error('Fetch Error: ', err.message);
+    }
 }
 
 function login(event) {
@@ -73,4 +62,28 @@ function login(event) {
         })
         .then(() => {window.location.href = "../app/index.html"})
         .catch((error) => console.error('Error: ', error));
+}
+
+async function login(event) {
+    const form = document.querySelector('#login_form');
+    event.preventDefault();
+    const formData = new FormData(form);
+    const dataObject = Object.fromEntries(formData.entries());
+    const jsonString = JSON.stringify(dataObject);
+    try {
+        let response = await fetch('https://tanuki.gay/api/v1/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonString,
+            credentials: 'include'
+        })
+        if (!response.ok) {
+            document.querySelector('#errorHandler').innerHTML = "Contact your local server tanuki with the following info: " + response.status;
+        }
+        window.location.href = "../app/index.html";
+    } catch (err) {
+        console.error("Could not log you in: ", error);
+    }
 }
