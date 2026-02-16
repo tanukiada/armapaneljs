3e0b66925536c00f12e391bbb0140e7cfd6b752c
ada taylor <admin@adataylor.io>
2026-02-15 18:23:08 -0600

    i really gotta stop following outdated tutorials

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ const config = {
 function authenticateToken(req, res, next) {
     const token = req.cookies.token;
 
-    if (!token) return res.redirect('/frontend/login');
+    if (!token) return res.status(401).json({ message: 'unauthorized' });
 
     try { 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (err) {
-        return res.redirect("/login")
+        return res.status(401).json({ message: "invalid token" });
     }
 }
 
